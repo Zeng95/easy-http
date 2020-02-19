d3a5531e9783da00a0289588df6233398671065f
Zeng Tao <qingchun1995@gmail.com>
2020-02-19 16:02:28 +0800

    create the post, put and delete method

--- app.js
+++ app.js
@@ -1,10 +1,61 @@
 const http = new EasyHTTP()
 
+// Create Data
+const data = {
+  name: 'Leo Zeng'
+}
+
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/users1', (err, posts) => {
-  if (err) {
-    console.log(err)
+http.get('http://5e4cdfb69b6805001438f033.mockapi.io/posts', (err, posts) => {
+  if (!err) {
+    // 如果没有 err 就显示 posts
+    console.log(JSON.parse(post))
   } else {
-    console.log(posts)
+    // 如果有 err 就显示 err
+    console.log(err)
+  }
+})
+
+// Get A Post
+http.get('http://5e4cdfb69b6805001438f033.mockapi.io/posts/1', (err, post) => {
+  if (!err) {
+    // 如果没有 err 就显示 posts
+    console.log(JSON.parse(post))
+  } else {
+    // 如果有 err 就显示 err
+    console.log(err)
+  }
+})
+
+// Create A Post
+http.post('http://5e4cdfb69b6805001438f033.mockapi.io/posts', data, (err, post) => {
+  if (!err) {
+    // 如果没有 err 就显示 posts
+    console.log(JSON.parse(post))
+  } else {
+    // 如果有 err 就显示 err
+    console.log(err)
+  }
+})
+
+// Update A Post
+http.put('http://5e4cdfb69b6805001438f033.mockapi.io/posts/1', data, (err, post) => {
+  if (!err) {
+    // 如果没有 err 就显示 posts
+    console.log(JSON.parse(post))
+  } else {
+    // 如果有 err 就显示 err
+    console.log(err)
+  }
+})
+
+// Delete a post
+http.delete('http://5e4cdfb69b6805001438f033.mockapi.io/posts/16', (err, msg) => {
+  if (!err) {
+    // 如果没有 err 就显示 posts
+    console.log(msg)
+  } else {
+    // 如果有 err 就显示 err
+    console.log(err)
   }
 })
--- easy-http.js
+++ easy-http.js
@@ -1,4 +1,5 @@
 function EasyHTTP() {
+  // 1. Create a new XMLHttpRequest object
   this.http = new XMLHttpRequest()
 }
 
@@ -9,7 +10,7 @@ EasyHTTP.prototype.get = function (url, callback) {
   // 2. Configure it: GET-request for the URL
   http.open('GET', url)
 
-  // 3. Send the request over the network  
+  // 3. Send the request over the network
   http.send()
 
   // 4. This will be called after the response is received
@@ -29,18 +30,78 @@ EasyHTTP.prototype.get = function (url, callback) {
 }
 
 // Make an HTTP POST Request
-EasyHTTP.prototype.post = (url) => {
-  this.http.open('POST', url)
+EasyHTTP.prototype.post = function (url, data, callback) {
+  const { http } = this
+
+  // 2. Configure it: POST-request for the URL
+  http.open('POST', url)
+  http.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+
+  // 3. Send the request over the network  
+  http.send(JSON.stringify(data))
+
+  // 4. This will be called after the response is received
+  http.onload = () => {
+    if (http.status === 201) {
+      callback(null, http.responseText)
+    } else {
+      callback(`Error: ${http.status}`)
+    }
+  }
+
+  http.onerror = () => {
+    alert('Request failed')
+  }
 }
 
 // Make an HTTP PUT Request
-EasyHTTP.prototype.put = (url) => {
-  this.http.open('PUT', url)
+EasyHTTP.prototype.put = function (url, data, callback) {
+  const { http } = this
+
+  // 2. Configure it: POST-request for the URL
+  http.open('PUT', url)
+  http.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+
+  // 3. Send the request over the network  
+  http.send(JSON.stringify(data))
+
+  // 4. This will be called after the response is received
+  http.onload = () => {
+    console.log(http)
+    if (http.status === 200) {
+      callback(null, http.responseText)
+    } else {
+      callback(`Error: ${http.status}`)
+    }
+  }
+
+  http.onerror = () => {
+    alert('Request failed')
+  }
 }
 
 // Make an HTTP DELETE Request
-EasyHTTP.prototype.delete = (url) => {
-  this.http.open('DELETE', url)
+EasyHTTP.prototype.delete = function (url, callback) {
+  const { http } = this
+
+  // 2. Configure it: GET-request for the URL
+  http.open('DELETE', url)
+
+  // 3. Send the request over the network  
+  http.send()
+
+  // 4. This will be called after the response is received
+  http.onload = () => {
+    if (http.status === 200) {
+      callback(null, 'Post is deleted successfully')
+    } else {
+      callback(`Error: ${http.status}`)
+    }
+  }
+
+  http.onerror = () => {
+    alert('Request failed')
+  }
 }
 
 
